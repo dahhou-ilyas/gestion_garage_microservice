2b316268c9c6607d72a63ef5794c151876e8fc68
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-10-30 21:59:13 +0100

    create router

---
+++ auth-service/src/routes/auth.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/auth.controller');
+
+const {authMiddleware,checkRole}=require('../middleware/auth.middleware');
+
+router.post('/register-employee'
+    ,authMiddleware
+    ,checkRole(['admin'])
+    ,authController.registerEmployee
+);
+
+router.post('login',authController.login);
+
+module.exports = router;
