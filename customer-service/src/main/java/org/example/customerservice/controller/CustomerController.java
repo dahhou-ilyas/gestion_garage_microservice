351e2777db09b859e79ac61f0d25cb2745f619d2
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-11-03 13:11:21 +0100

    create controller

---
+++ customer-service/src/main/java/org/example/customerservice/controller/CustomerController.java
@@ -0,0 +1,31 @@
+package org.example.customerservice.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.customerservice.dto.CustomerDTO;
+import org.example.customerservice.service.CustomerService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/customers")
+@RequiredArgsConstructor
+public class CustomerController {
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<CustomerDTO> createCustomer(@RequestBody CustomerDTO customerDTO) {
+        return ResponseEntity.ok(customerService.createCustomer(customerDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CustomerDTO> updateCustomer(
+            @PathVariable Long id,
+            @RequestBody CustomerDTO customerDTO) {
+        return ResponseEntity.ok(customerService.updateCustomer(id, customerDTO));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CustomerDTO> getCustomer(@PathVariable Long id) {
+        return ResponseEntity.ok(customerService.getCustomerById(id));
+    }
+}
