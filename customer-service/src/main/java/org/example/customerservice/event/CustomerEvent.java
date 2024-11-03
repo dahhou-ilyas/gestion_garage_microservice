6753cf66ea009c40c5d3715a04a3ed766335bd0c
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-11-03 11:26:56 +0100

    add dto and event

---
+++ customer-service/src/main/java/org/example/customerservice/event/CustomerEvent.java
@@ -0,0 +1,10 @@
+package org.example.customerservice.event;
+
+import lombok.Data;
+
+@Data
+public class CustomerEvent {
+    private String eventType;
+    private Long customerId;
+    private String customerEmail;
+}
