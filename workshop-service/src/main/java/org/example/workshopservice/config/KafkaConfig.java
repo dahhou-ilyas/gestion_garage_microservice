afada1f386f1e888b4621a9b6e6540887d4e4224
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-11-17 23:18:30 +0100

    create config server

--- workshop-service/src/main/java/org/example/workshopservice/config/KafkaConfig.java
+++ workshop-service/src/main/java/org/example/workshopservice/config/KafkaConfig.java
@@ -1,9 +1,10 @@
 package org.example.workshopservice.config;
 
-import lombok.Value;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -11,6 +12,7 @@ import org.springframework.kafka.core.ProducerFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+@Configuration
 public class KafkaConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
