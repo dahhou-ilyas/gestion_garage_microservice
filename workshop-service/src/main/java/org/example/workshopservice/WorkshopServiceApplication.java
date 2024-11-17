1d7a7d1b15d12ab403320e64618ba243afc7897e
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-11-17 20:10:35 +0100

    config

--- workshop-service/src/main/java/org/example/workshopservice/WorkshopServiceApplication.java
+++ workshop-service/src/main/java/org/example/workshopservice/WorkshopServiceApplication.java
@@ -2,8 +2,10 @@ package org.example.workshopservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class WorkshopServiceApplication {
 
     public static void main(String[] args) {
