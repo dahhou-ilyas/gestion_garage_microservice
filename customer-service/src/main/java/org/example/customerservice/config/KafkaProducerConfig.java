019a394c2edc0d2b6ab07527dcc5b4fbac8e0944
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-11-17 19:33:01 +0100

    config

--- customer-service/src/main/java/org/example/customerservice/config/KafkaProducerConfig.java
+++ customer-service/src/main/java/org/example/customerservice/config/KafkaProducerConfig.java
@@ -3,6 +3,7 @@ package org.example.customerservice.config;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.example.customerservice.event.CustomerEvent;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -15,10 +16,14 @@ import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); // Assurez-vous d'utiliser l'adresse correcte
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers); // Assurez-vous d'utiliser l'adresse correcte
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class); // Utilisez JsonSerializer si CustomerEvent est un objet JSON
         return new DefaultKafkaProducerFactory<>(configProps);
