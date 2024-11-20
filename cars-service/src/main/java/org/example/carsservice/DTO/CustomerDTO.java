c5e619a71c04e43c8ae04d7f7edc8d7ab43600b5
dahhou.ilyas <dahhou.ilyas@gmail.com>
2024-11-20 18:30:55 +0100

    addapt

--- cars-service/src/main/java/org/example/carsservice/DTO/CustomerDTO.java
+++ cars-service/src/main/java/org/example/carsservice/DTO/CustomerDTO.java
@@ -1,8 +1,12 @@
 package org.example.carsservice.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustomerDTO {
     private Long id;
     private String identityNumber;
